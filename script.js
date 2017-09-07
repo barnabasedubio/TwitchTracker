75aa3d2cc1b55fa199a172944ff0b88ad6c1942b
Barnabas Edubio <barny.edubio@gmail.com>
2017-09-07 20:27:57 +0200

    figured out how to retrieve stream data

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// api-URL: https://wind-bow.glitch.me/twitch-api/streams/[streamer_name]
+var listOfStreamers =
+    ["summit1g", "lirik", "cohhcarnage",
+    "freecodecamp", "imaqtpie", "rocketleague",
+    "shlorox", "noobs2ninjas", "drunkdevs"];
+
+var streamernames = [];
+
+var apiURL = "https://wind-bow.glitch.me/twitch-api/streams/";
+var streamData;
+var testURL = apiURL + "lirik";
+
+$(document).ready(function () {
+    console.log("url: " + testURL);
+    $.getJSON(testURL, function (data) {
+        //console.log(data);
+        lirik = data;
+        console.log(lirik.stream.game);
+    });
+});
+
+function getStreamData() {
+    
+}
